c08bb78bd9f4bc21c93cbad153807ba7aad08742
Yolann Sabaux <yolann.sabaux@gmail.com>
2021-05-03 14:45:16 +0200

    test yo

---
+++ code/crawl_list.py
@@ -0,0 +1,39 @@
+from bs4 import BeautifulSoup
+import requests
+import csv
+
+source = requests.get("https://www.immoweb.be/en/search/house-and-apartment/for-sale?countries=BE&page=1&orderBy=relevance).text
+
+soup = BeautifulSoup(source, 'lxml')
+
+print(soup.prettify())
+# csv_file = open('test.csv', "w")
+
+# csv_writer = csv.writer(csv_file)
+# csv_writer.writerow(["headline", "summary", "video_link"])
+
+# for article in soup.find_all("article"):
+
+#     # print(article.prettify())
+
+#     headline = article.h2.a.text
+#     print(headline)
+
+#     summary = article.find("div", class_ ="entry-content").p.text
+#     print(summary)
+
+#     try:
+#         vid_src = article.find("iframe", class_="youtube-player")["src"]
+
+#         vid_id = vid_src.split("/")[4]
+#         vid_id = vid_id.split("?")[0]
+
+#         yt_link = f'https://youtube.com/watch?v={vid_id}'
+#     except  Exception as e:
+#         yt_link = None
+    
+#     print(yt_link)
+#     print()
+
+#     csv_writer.writerow([headline, summary, yt_link])
+# csv_file.close()
