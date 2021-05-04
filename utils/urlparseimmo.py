d16072a07ac5e7e45844766a0361a42d8fe6e05d
Ke Thien <kethien.ng@gmail.com>
2021-05-04 10:21:41 +0200

    add selenium because not all elements where scrap by beautifulsoup only (javascript)

--- utils/urlparseimmo.py
+++ utils/urlparseimmo.py
@@ -1,7 +1,10 @@
 from urllib.parse import urlparse
+from selenium import webdriver
 from bs4 import BeautifulSoup
 import requests
 import re
+
+driver = webdriver.Chrome()
 
 
 def urlparseimmo(url) -> dict:
@@ -18,15 +21,18 @@ def urlparseimmo(url) -> dict:
     segments = parsed.path[1:-1].split("/")
 
     # Beautifulsoup to get the rest of the info
-    r = requests.get(url)
-    soup = BeautifulSoup(r.content, 'lxml')
+    # r = requests.get(url)
+    driver.get(url)
+    html = driver.page_source
+    soup = BeautifulSoup(html, 'html.parser')
 
     # PRICE:
     price = soup.select('p.classified__price .sr-only')[0].text[:-1]
     price = re.search('\d+', price).group()
 
     # TODO: Type of SALE -> string
-
+    type_of_sale = soup.find_all('h2')
+    print(type_of_sale)
     # TODO: Number of rooms -> int
 
     # TODO: Area -> int
@@ -63,7 +69,7 @@ def urlparseimmo(url) -> dict:
         'type_of_property': "apartment" if segments[2] in subtype_apt else "house",
         'price': price
     }
-
+    driver.quit()
     return d
 
 
