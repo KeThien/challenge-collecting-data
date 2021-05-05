a1d8916859b308a4ba7a058f222e9df0ade01698
Yolann Sabaux <yolann.sabaux@gmail.com>
2021-05-05 17:49:55 +0200

    common work

--- fulldata_extraction.py
+++ fulldata_extraction.py
@@ -1,1 +1,7 @@
 from utils.urlparseimmo import urlparseimmo
+
+
+url = 'https://www.immoweb.be/en/classified/house/for-sale/woluwe-saint-pierre/1150/9310094?searchId=60929d137685e'
+infos_in_url = urlparseimmo(url)
+
+print(infos_in_url)
--- main/snippet2.py
+++ main/snippet2.py
@@ -10,24 +10,24 @@ from bs4 import BeautifulSoup
 PATH = "/opt/chromedriver" 
 driver = webdriver.Chrome(PATH)
 
-url = "https://www.immoweb.be/en/classified/mixed-use-building/for-sale/liege/4000/9316691?"
-# url = "https://www.immoweb.be/en/classified/house/for-sale/sint-pieters-leeuw/1600/9310259?searchId=60929f813c19a" 
+# url = "https://www.immoweb.be/en/classified/mixed-use-building/for-sale/liege/4000/9316691?"
+# # url = "https://www.immoweb.be/en/classified/house/for-sale/sint-pieters-leeuw/1600/9310259?searchId=60929f813c19a" 
 
-driver.get(url)
-html = driver.page_source
-soup = BeautifulSoup(html, 'html.parser')
+# driver.get(url)
+# html = driver.page_source
+# soup = BeautifulSoup(html, 'html.parser')
 
-# Number of rooms -> int
-rooms_and_area = soup.select('p.classified__information--property')[0].text
-rooms = re.findall("([0-9]+)", rooms_and_area)[0]
-print(rooms)
+# # Number of rooms -> int
+# rooms_and_area = soup.select('p.classified__information--property')[0].text
+# rooms = re.findall("([0-9]+)", rooms_and_area)[0]
+# print(rooms)
 
 
 
-#Furnished -> boolean
-furnished = soup.find("th", string = "Furnished")
-furnished = furnished.find_next_sibling().contents[0].strip()
-print(furnished)
+# #Furnished -> boolean
+# furnished = soup.find("th", string = "Furnished")
+# furnished = furnished.find_next_sibling().contents[0].strip()
+# print(furnished)
 
 
 
