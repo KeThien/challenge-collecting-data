47debdc4f6b65e159ad7a830a9ff4b8a8177584b
Ke Thien <kethien.ng@gmail.com>
2021-05-05 14:22:39 +0200

    fix text

--- utils/urlparseimmo.py
+++ utils/urlparseimmo.py
@@ -37,20 +37,20 @@ def urlparseimmo(url) -> dict:
     else:
         type_of_sale = "private"
 
-    # TODO: Number of rooms -> int
+    # Number of rooms -> int
     rooms_and_area = soup.select('p.classified__information--property')[0].text
-    rooms = re.findall("([0-9]+)",rooms_and_area)[0]
-    
-    # TODO: Area -> int
-    area = re.findall("([0-9]+)",rooms_and_area)[1]
-    
+    rooms = re.findall("([0-9]+)", rooms_and_area)[0]
+
+    # Area -> int
+    area = re.findall("([0-9]+)", rooms_and_area)[1]
+
     # Fully equipped kitchen -> boolean (Directly pick from the dataLayer)
 
     # TODO: Furnished -> boolean
 
     # TODO: Open fire -> boolean
 
-    # TODO: Terrace -> Boolean if True: Area -> int
+    # Terrace -> Boolean if True: Area -> int
     if dataLayer["classified"]["outdoor"]["terrace"]["exists"]:
         th_terrace = soup.find('th', string='Terrace surface')
         terrace_surface = th_terrace.find_next_sibling(
@@ -59,12 +59,12 @@ def urlparseimmo(url) -> dict:
         terrace = int(terrace_surface)
     else:
         terrace = None
+
     # TODO: Garden -> Boolean if True: Area -> int
 
     # TODO: Surface of the land -> int
     surface_land = soup.select('span.overview__text')[3].text
-    surface_land = re.findall("([0-9]+)",surface_land)[0]
-   
+    surface_land = re.findall("([0-9]+)", surface_land)[0]
 
     # TODO: Surface area of the plot of land -> int
     surface_plot = soup.select('th.classified-table__header')
@@ -79,14 +79,14 @@ def urlparseimmo(url) -> dict:
     else:
         hasSwimmingPool = False
 
-    # State of the building (New, to be renovated, ...) -> string
+    # State of the building (New, to be renovated, ...) -> string (dataLayer)
 
     # TODO: Cleaning
 
     # TODO: Finalizing
 
     # MERGE ALL INFOS in a DICT to return
- 
+
     d = {
         'id': int(dataLayer["classified"]["id"]),
         'locality': int(dataLayer["classified"]["zip"]),
@@ -99,8 +99,8 @@ def urlparseimmo(url) -> dict:
         'equipped_kitchen': dataLayer["classified"]["kitchen"]["type"],
         'terrace': terrace,
         'rooms': int(rooms),
-        'area (m²)': int(area, ),
-        'surface_of_land (m²)': int(surface_land, )
+        'area_m2': int(area, ),
+        'surface_of_land_m2': int(surface_land, )
     }
     driver.quit()
     return d
